11962ad8b1bcbc4c906ca2d2b22cf5f770462b72
SanthoshVarmaThamishetty <156236858+SanthoshVarmaThamishetty@users.noreply.github.com>
2026-02-17 13:47:46 -0500

    Update main.py

--- RetailAgent/main.py
+++ RetailAgent/main.py
@@ -129,11 +129,3 @@ def query(q: str):
         "answer": answer
     }
 
-
-# ==========================
-# Render / Production Start
-# ==========================
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
